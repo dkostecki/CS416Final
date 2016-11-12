f220e8114c8ba73d2d78c2e0b35af186012bd1ba
Daniel Kostecki <danielkostecki@my.ccsu.edu>
2016-11-12 19:22:25 +0000

    added validations for descriptions maximum 200

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+    validates:description, length{maximum:200}
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+    validates:description,length:{maximum:200}
+    validates:name, length:{maximum:30}
 end
