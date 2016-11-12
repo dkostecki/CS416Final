abe1121791f9156c2f39dc70c2e319eeb8422930
Daniel Kostecki <danielkostecki@my.ccsu.edu>
2016-11-12 19:29:20 +0000

    added data associations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,4 @@
 class Cart < ApplicationRecord
+    has_many :user
+    has_many :product
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
     validates:description,length:{maximum:200}
     validates:name, length:{maximum:30}
+    belongs_to :cart
+    belongs_to :category
 end
