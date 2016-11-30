c421758a8fb2c05a58cbd972dce15b22f5c356e4
Daniel Kostecki <danielkostecki@my.ccsu.edu>
2016-11-30 16:43:31 +0000

    added password_digest to users and migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161112191158) do
+ActiveRecord::Schema.define(version: 20161130164203) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
@@ -38,8 +38,9 @@ ActiveRecord::Schema.define(version: 20161112191158) do
   create_table "users", force: :cascade do |t|
     t.text     "username"
     t.text     "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
