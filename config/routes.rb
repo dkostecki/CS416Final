6998c7162a56c05d1455cb0edbbe39c95d7a4038
Thi Nguyen <thi.nguyen@my.ccsu.edu>
2016-12-01 06:06:51 +0000

    Worked on adding a common header, not done yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get 'sessions/new'
+
   resources :carts
   resources :users
   resources :products
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'users#new'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
 end
