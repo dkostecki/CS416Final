27a961f6779618f4d8860b5d97b6461fdef8f6c1
Thi Nguyen <thi.nguyen@my.ccsu.edu>
2016-12-01 19:59:41 +0000

    Login works. When a user logs in, a dropdown menu on the top right is shown with other options. Not all options will go somewhere.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,23 @@
 module SessionsHelper
+#Logs in the given user
+  def log_in(user)
+     session[:user_id] = user.id
+  end
+  
+#Returns current logged-in user  
+  def current_user
+     @current_user ||= User.find_by(id: session[:user_id])
+  end  
+  
+#Returns true if the user is logged in, otherwise it returns false  
+  def logged_in?
+     !current_user.nil?
+  end
+  
+#Logs current user out
+  def log_out
+     session.delete(:user_id)
+     @current_user=nil
+  end
+  
 end
