f81d8fc74ad8c8a640abd69f9c3ed1fbe37525e5
Daniel Kostecki <danielkostecki@my.ccsu.edu>
2016-12-02 23:09:26 +0000

    added check for login: must be logged in to hit /users, must be correct user to access records

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index,:show,:edit, :update]
+  before_action :correct_user, only: [:show,:edit, :update]
 
   
   # GET /users
@@ -17,6 +18,7 @@ class UsersController < ApplicationController
     
   end
 
+#
   def correct_user
     @user = User.find(params[:id])
     unless @user == current_user
@@ -24,6 +26,14 @@ class UsersController < ApplicationController
       redirect_to(root_url)
     end
   end
+  
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
   
   # GET /users/new
   def new
