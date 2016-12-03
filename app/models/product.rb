efde9be170ed32c64e0b2159e0a4e2fd1634578a
Daniel Kostecki <danielkostecki@my.ccsu.edu>
2016-12-03 03:09:50 +0000

    small picture size

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,11 @@ class Product < ApplicationRecord
     validates:name, length:{maximum:30}
     belongs_to :category
     
-    has_attached_file :photo
+    has_attached_file :photo,
+    :styles => {
+      :thumb => "100x100#",
+      :small  => "150x150>",
+      :medium => "200x200" }
+      
     do_not_validate_attachment_file_type :photo
 end
