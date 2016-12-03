6ef78e568fff37490192e6018c3aa5ba81bf5a45
Daniel Kostecki <danielkostecki@my.ccsu.edu>
2016-12-03 02:04:09 +0000

    fixed redundant code in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,13 +27,6 @@ class UsersController < ApplicationController
     end
   end
   
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
-
   
 # Confirms a logged-in user.
   def logged_in_user
