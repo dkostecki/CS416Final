51ea1a63b8a0216f050d4e8e872b38f5b5d4a315
Daniel Kostecki <danielkostecki@my.ccsu.edu>
2016-12-03 00:28:31 +0000

    validates uniqueness of username and length of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true,length: { minimum: 6 },
     allow_nil: true
+    validates :username, uniqueness: true, length: { minimum: 3 }
 end
