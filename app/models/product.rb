464e77270b8f1335248eccb009ae12b7717b58a2
Thi Nguyen <thi.nguyen@my.ccsu.edu>
2016-12-03 02:21:36 +0000

    Added paperclip gem and a file field to upload pictures. It doesn't do anything yet

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
     validates:description,length:{maximum:200}
     validates:name, length:{maximum:30}
     belongs_to :category
+    
+    has_attached_file :photo
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130164203) do
+ActiveRecord::Schema.define(version: 20161203021711) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
@@ -31,8 +31,12 @@ ActiveRecord::Schema.define(version: 20161130164203) do
     t.integer  "category_id"
     t.integer  "price"
     t.text     "name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
